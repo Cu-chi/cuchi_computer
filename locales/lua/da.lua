55330414d0e87c138ccfff087075586037082c31
Cu-chi <jusaumsp@gmail.com>
2024-04-05 01:35:48 +0200

    chore(locales): create danish locale
    re #4

---
+++ locales/lua/da.lua
@@ -0,0 +1,7 @@
+Locales["DA"] = {
+    start_computer = "~INPUT_CONTEXT~ for at starte computer",
+    already_in_use = "Denne computer er allerede i brug.",
+    in_vehicle = "Bærbare computere kan ikke bruges i et køretøj.",
+    data_heist = "Datakup",
+    data_heist_call = "Datakup i gang, GPS indstillet til at finde position.",
+}
