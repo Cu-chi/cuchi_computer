5959f9a424678191d67ea1f3057961e71dea077a
Cu-chi <jusaumsp@gmail.com>
2024-04-05 01:26:33 +0200

    chore(locales): create spanish locale
    closes #3

---
+++ locales/lua/es.lua
@@ -0,0 +1,7 @@
+Locales["ES"] = {
+    start_computer =  "~INPUT_CONTEXT~ para iniciar el ordenador",
+    already_in_use =  "Este ordenador ya está en uso.",
+    in_vehicle =  "Las portátiles no pueden usarse en un vehículo.",
+    data_heist =  "Robo de Datos",
+    data_heist_call =  "Robo de Datos en progreso, GPS configurado a la posición localizada."
+}
